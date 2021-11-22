58a178e263e6f8eebaaa1f7b7322539d23175b76
allanbraun <allanvobraun@gmail.com>
2021-11-21 23:59:33 -0300

    configuracoes do rabbit

--- apps/consumer/src/main.ts
+++ apps/consumer/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ConsumerModule } from './consumer.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ConsumerModule);
-  await app.listen(process.env.PORT_CONSUMER);
+  const configService = app.get(ConfigService);
+
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
--- apps/producer/src/producer.module.ts
+++ apps/producer/src/producer.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ProducerController } from './producer.controller';
-import { ProducerService } from './producer.service';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { ProducerController } from "./producer.controller";
+import { ProducerService } from "./producer.service";
+import { ConfigModule } from "@nestjs/config";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ClientsModule.register([
+    {
+      name: "VEHICLE_PRODUCER",
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}`],
+        queue: process.env.RABBITMQ_QUEUE_NAME,
+        queueOptions: {
+          durable: true
+        }
+      }
+    }
+  ])],
   controllers: [ProducerController],
-  providers: [ProducerService],
+  providers: [ProducerService]
 })
-export class ProducerModule {}
+export class ProducerModule {
+}
